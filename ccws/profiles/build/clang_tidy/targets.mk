d6d794f40a43902524cb6a97c106540699dd77dc
Alexander Sherikov <alexander@sherikov.net>
2025-05-22 22:13:59 +0400

    profiles/clang_tidy: use clang
    clang-tidy differs from gcc in the same way as clang, so it might be
    necessary to apply the same compiler specific workarounds, i.e,
    clang-tidy/gcc combo may not work.

--- ccws/profiles/build/clang_tidy/targets.mk
+++ ccws/profiles/build/clang_tidy/targets.mk
@@ -1,3 +1,3 @@
 bp_clang_tidy_install_build: bp_common_install_build
-	bash -c "${SETUP_SCRIPT}; sudo ${APT_INSTALL} clang-tidy-\$${CCWS_LLVM_VERSION}"
+	bash -c "${SETUP_SCRIPT}; sudo ${APT_INSTALL} clang-tidy-\$${CCWS_LLVM_VERSION} clang-\$${CCWS_LLVM_VERSION}"
 
