03f6b3dbeb44fb1c9d1cc0d7e93bf58cf9ad857c
Alexander Sherikov <alexander@sherikov.net>
2025-08-15 20:27:43 +0400

    profiles/deb: fix dependency list generation

--- ccws/profiles/build/deb/bin/control.sh
+++ ccws/profiles/build/deb/bin/control.sh
@@ -1,15 +1,18 @@
 #!/bin/bash
 
 set -e
+set -o pipefail
 
-# TODO no need to depend on build dependencies
 cat > "${CCWS_DEBIAN_DIR}/control" <<EOF
-Package: $(echo "${CCWS_PKG_FULL_NAME}" | sed 's/_/-/g')
-Version: $(sed 's/_/-/g' < "${CCWS_DEB_INFO_DIR}/version_hash.txt")
+Package: $(echo "${CCWS_PKG_FULL_NAME}" | sed 's/_/-/g' || ERR=YES)
+Version: $(sed 's/_/-/g' < "${CCWS_DEB_INFO_DIR}/version_hash.txt" || ERR=YES)
 Architecture: ${CCWS_DEB_ARCH}
 Maintainer: ${AUTHOR} <${EMAIL}>
 Description: ${VENDOR} ${PKG}
-Depends: $(paste -s -d ',' < "${WORKSPACE_DIR}/build/${CCWS_BUILD_PROFILES_ID}_dep/deps_${PKG_ID}.deb")
-Installed-Size: $(du -s "${CCWS_INSTALL_DIR_BUILD_ROOT}" | cut -f 1)
+Depends: $(paste -s -d ',' < "${DEPLIST_FILE}.deb" || ERR=YES)
+Installed-Size: $(du -s "${CCWS_INSTALL_DIR_BUILD_ROOT}" | cut -f 1 || ERR=YES)
 EOF
 
+# errors in process subsitution are lost
+# https://stackoverflow.com/questions/79657285/how-to-detect-errors-with-process-substitution
+test "${ERR}" != "YES" || exit 255
